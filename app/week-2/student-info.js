612028b9f3aa6f98bc8a322c067dbd25b537c063
Ryan <ryansheikh003@gmail.com>
2024-01-24 20:31:32 -0700

    Fixed error from student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -5,7 +5,7 @@ function StudentInfo() {
     return (
         <>
             <p>Student Name: Raiyan Anwar</p>
-            <p>Github repository<Link href="https://github.com/Raiyan03/CPRG306-ASSIGNMENTS.git">"https://github.com/Raiyan03/CPRG306-ASSIGNMENTS.git"</Link></p>
+            <Link href="https://github.com/Raiyan03/CPRG306-ASSIGNMENTS.git">https://github.com/Raiyan03/CPRG306-ASSIGNMENTS.git</Link>
         </>
     );
 }
