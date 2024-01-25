ce88dd4bfc4afb8ce63cb6911e6c0b507910fc40
Ryan <ryansheikh003@gmail.com>
2024-01-24 19:23:03 -0700

    Initial commit: Rendered student info

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./student-info";
+
+export default function Page() {
+    return (
+      <main class = "h-screen">
+        <h1>Shopping List</h1>   
+        <StudentInfo/>
+      </main>
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+
+function StudentInfo() {
+    return (
+        <>
+            <p>Student Name: Raiyan Anwar</p>
+            <p>Github repository<Link href="https://github.com/Raiyan03/CPRG306-ASSIGNMENTS.git">"https://github.com/Raiyan03/CPRG306-ASSIGNMENTS.git"</Link></p>
+        </>
+    );
+}
+
+export default StudentInfo;
