7cc2f68263054c02ffc6045f1e2abc4f66130fb7
Ryan <ryansheikh003@gmail.com>
2024-02-09 15:28:02 -0700

    Assignment: Week4 completed

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ export default function Home() {
         <h1 class="text-3xl">CPRG 306: Web Development 2- Assignments</h1>
         <Link href="week-2"><p className="text-xl mt-3" >Week 2</p></Link>
         <Link href="week-3"><p className="text-xl mt-3" >Week 3</p></Link>
+        <Link href="week-4"><p className="text-xl mt-3" >Week 4</p></Link>
     </main>
   );
 }
---
+++ app/week-4/page.js
@@ -0,0 +1,9 @@
+import NewItem from './new-item'
+
+export default function week4(){
+    return(
+        <div className='flex justify-center h-screen bg-black  text-white overflow-y-auto'>
+            <NewItem/>
+        </div>
+    )
+}
